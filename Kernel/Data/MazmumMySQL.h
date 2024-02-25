d45779366ddc55cbc40452d19ca2eaf56826c6bc
ALi <Sayavdera@protonmail.com>
2024-02-25 22:32:01 +0500

    Updating DataBase Structure

--- Kernel/Data/MazmumMySQL.h
+++ Kernel/Data/MazmumMySQL.h
@@ -8,8 +8,51 @@
 
 #include "../AlGhadab/MazmumMod.h"
 
+#define DEFAULT_DB "mysql"
+
+#ifndef LIBMYSQLCLIENT
+#else
+#if defined(MAZMUM_HAVE_MYSQL_MYSQL_H)
+#include <mysql/mysql.h>
+#elif defined(MAZMUM_HAVE_MYSQL_J)
+#include <mysql.h>
+#else
+#error LibMySQLClient found, but not usable headers avialabel
+#endif
+MYSQL *mysql = NULL;
+#endif
+
 void MAZMUM_DUMMY_MYSQL(void);
 void MAZMUM_MYSQL_SERVICE(char *IP, int32_t PORT, unsigned char Options, int32_t S, char *MiscPTR, FILE *Fp, char *HostName);
+void MAZMUM_HASH_PASSWORD(unsigned long *Result, const char *Password);
+
+char *MAZMUM_SCRAMBLE(char *To, const char *Message, const char *Password);
+
+
+extern int32_t MAZMUM_INTERNAL_RECV(int32_t Socket, char *Buf, int32_t Length);
+extern int32_t MAZMUM_DATA_READY_TIMED(int32_t Socket, long *Second, long NanoSecond);
+
+extern int32_t MAZMUM_DATA_READY_TIMED(int32_t Socket, long Second, long NanoSecond);
+extern MAZMUM_OPTION __MAZMUM_OPTION_T;
+extern char *MAZMUM_EXID;
+char MySQLSALT[9];
+
+void MAZMUM_MYSQL_SERVICE(char *IP, int32_t PORT, unsigned char Options, int32_t S, char *MiscPTR, FILE *Fp, char *HostName);
+void MAZMUM_RANDOM_INIT(struct MAZMUM_RAND_STRUCT *Rand_Str, unsigned long Seed_One, unsigned long Seed_Two);
+void MAZMUM_HASH_PASSWORD(unsigned long *Result, const char *Password);
+void MAZMUM_MYSQL_USAGE(const char *Service);
+
+char *MAZMUM_MYSQL_RECEIVE_LINE(int32_t Socket);
+char *MAZMUM_MYSQL_PREPARE_AUTH(char *Login, char *Password);
+
+char MAZMUM_MYSQL_INIT(int32_t Socket);
+char MAZMUM_MYSQL_PARSE_RESPONSE(unsigned char *Response);
+char MAZMUM_MYSQL_SEND_COM_QUIT(int32_t Socket);
+char *MAZMUM_SCRAMBLE(char *To, const char *Message, const char *Password);
+
+int32_t MAZMUM_MYSQL_START(char *IP, int32_t PORT, unsigned char Options, int32_t Socket, char *MiscPTR, FILE *Fp);
+
+double MAZMUM_RND(struct MAZMUM_RAND_STRUCT *Rand_Str);
 
 // #else
 
