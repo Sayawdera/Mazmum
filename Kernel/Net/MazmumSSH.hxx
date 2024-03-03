1efe1c364930ad63d73344760a466cc515daa7b0
Ali <Sayavdera@protonmail.com>
2024-03-03 10:47:34 +0500

    Updating

--- Kernel/Net/MazmumSSH.hxx
+++ Kernel/Net/MazmumSSH.hxx
@@ -1,8 +1,29 @@
-//
-// Created by Core VOID on 25.02.2024.
-//
-
 #ifndef MAZMUM_MAZMUMSSH_HXX
 #define MAZMUM_MAZMUMSSH_HXX
 
-#endif //MAZMUM_MAZMUMSSH_HXX
+
+#include "../AlGhadab/MazmumMod.hxx"
+
+#include <atomic>
+#include <libssh/libssh.h> // libssh.org for C++
+
+
+SSHSession Session = NULL;
+
+extern MAZMUM_OPTION __Mazmum_Option;
+char *MAZMUM_EXIT;
+char *Buf;
+std::atomic_int32_t NewSession = 1;
+
+void MazmumDummySSH();
+
+void MazmumServiceSSH(char *IP, int32_t PORT, unsigned char Option, int32_t Sp, char *MiscPTR, FILE *Fp, char *HostName);
+void MazmumServiceSSHKey(char *IP, int32_t PORT, unsigned char Option, int32_t Sp, char *MiscPTR, FILE *Fp, char *HostName);
+
+std::atomic_int32_t MazmumStartSSH(char *IP, int32_t PORT, unsigned char Option, int32_t Sp, char *MiscPTR, FILE *Fp);
+std::atomic_int32_t MazmumSSHKey(char *IP, int32_t PORT, unsigned char Option, int32_t Sp, char *MiscPTR, FILE *Fp);
+std::atomic_int32_t MazmumServiceSSH_INIT(char *IP, int32_t PORT, unsigned char Option, int32_t Sp, char *MiscPTR, FILE *Fp, char *HostName);
+std::atomic_int32_t MazmumServiceSSHKey_INIT(char *IP, int32_t PORT, unsigned char Option, int32_t Sp, char *MiscPTR, FILE *Fp, char *HostName);
+
+
+#endif MAZMUM_MAZMUMSSH_HXX
